8125ed3b984f70cefd6e7b690e0a68d1c9faa16b
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-08-08 16:58:12 +0300

    задания циклы

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,9 @@ public class Main {
         for(; currentNum > 0; currentNum--) {
             System.out.print(currentNum + " ");
         }
+        int firstFriday = 5;
+        for (int currentFriday = firstFriday; currentFriday <= 31; currentFriday += 7 ) {
+            System.out.println("Сегодня пятница, " + currentFriday + "-е число. Необходимо подготовить отчет.");
+        }
         }
     }
