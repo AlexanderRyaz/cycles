61143c16ead71dd77d379595b6892b6373bea834
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-08-08 15:46:14 +0300

    задания циклы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int currentNum = 0;
+        while (currentNum < 10) {
+            System.out.print(++currentNum + " ");
+        }
+        System.out.println();
+
+        for(; currentNum > 0; currentNum--) {
+            System.out.print(currentNum + " ");
+        }
+        }
     }
-}
